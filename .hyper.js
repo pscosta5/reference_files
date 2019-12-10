e1e8af6641b4c2dea461127e418a1d2a46edc222
Paulo S. Costa <pauloscosta5@gmail.com>
2019-12-10 09:49:21 -0800

    Add comments to hyper config

--- .hyper.js
+++ .hyper.js
@@ -166,7 +166,12 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-search", "hyper-material-theme", "hyperlinks", "hyper-alt-click", ],
+  plugins: [
+    "hyper-search",
+    "hyper-material-theme",
+    "hyperlinks",
+    "hyper-alt-click"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
