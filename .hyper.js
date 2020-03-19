3653506928b012dc06574f958336ea955d019b1c
Paulo S. Costa <pauloscosta5@gmail.com>
2020-03-19 15:52:14 -0700

    Add hyper statusline and hypercwd

--- .hyper.js
+++ .hyper.js
@@ -166,7 +166,14 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-search", "hyper-material-theme", "hyperlinks", "hyper-alt-click"],
+  plugins: [
+    "hyper-search",
+    "hyper-material-theme",
+    "hyperlinks",
+    "hyper-alt-click",
+    "hyper-statusline",
+    "hypercwd"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
