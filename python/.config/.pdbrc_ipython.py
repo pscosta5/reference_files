233c99c6cf16a5c2a6fb3b1886e4e38f57b80dbd
Paulo S. Costa <pauloscosta5@gmail.com>
2020-12-30 14:03:35 -0800

    [Python] copy ipython config to pdbrc

--- python/.config/.pdbrc_ipython.py
+++ python/.config/.pdbrc_ipython.py
@@ -6,5 +6,12 @@ except ImportError:
     pass
 else:
     cfg = traitlets.config.get_config()
-    cfg.InteractiveShellEmbed.colors = "Linux"  # syntax highlighting
+    # Copy config from ipython_config.py
+    cfg.InteractiveShellEmbed.color_info = True
+    cfg.InteractiveShellEmbed.colors = "Linux"
     cfg.InteractiveShellEmbed.confirm_exit = False
+    cfg.InteractiveShellEmbed.autoformatter = "black"
+    cfg.InteractiveShellEmbed.highlight_matching_brackets = True
+    cfg.InteractiveShellEmbed.highlighting_style = "stata-dark"
+    cfg.InteractiveShellEmbed.true_color = True
+
