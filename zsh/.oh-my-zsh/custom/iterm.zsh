0dfa1085077558756b86ef5d9a1069c9dc9da56a
Paulo S. Costa <pauloscosta5@gmail.com>
2021-02-04 09:33:56 -0800

    [iTerm] move config to startup file

---
+++ zsh/.oh-my-zsh/custom/iterm.zsh
@@ -0,0 +1,4 @@
+# iTerm integration
+# https://iterm2.com/
+# curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
