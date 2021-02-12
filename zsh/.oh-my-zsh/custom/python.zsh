566d892e84823255594f769e120a9b0a1e26421a
Paulo S. Costa <pauloscosta5@gmail.com>
2021-02-12 15:23:33 -0800

    [Python] move build packages to startup file

--- zsh/.oh-my-zsh/custom/python.zsh
+++ zsh/.oh-my-zsh/custom/python.zsh
@@ -1,3 +1,25 @@
 # Python
 # Python REPL startup file
 export PYTHONSTARTUP=$HOME/.ipython/profile_default/startup/rich_rendering.py
+
+# Brew build packages for Python using pyenv
+# openssl
+# brew install openssl
+LDFLAGS="-L/usr/local/opt/openssl@1.1/lib $LDFLAGS"
+CPPFLAGS="-I/usr/local/opt/openssl@1.1/include $CPPFLAGS"
+PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig $PKG_CONFIG_PATH"
+# readline
+# brew install readline
+LDFLAGS="-L/usr/local/opt/readline/lib $LDFLAGS"
+CPPFLAGS="-I/usr/local/opt/readline/include $CPPFLAGS"
+PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig $PKG_CONFIG_PATH"
+# sqlite3
+# brew install sqlite3
+LDFLAGS="-L/usr/local/opt/sqlite/lib $LDFLAGS"
+CPPFLAGS="-I/usr/local/opt/sqlite/include $CPPFLAGS"
+PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig $PKG_CONFIG_PATH"
+# zlib
+# brew install zlib
+export LDFLAGS="-L/usr/local/opt/zlib/lib $LDFLAGS"
+export CPPFLAGS="-I/usr/local/opt/zlib/include $CPPFLAGS"
+export PKG_CONFIG_PATH="/usr/local/opt/zlib/lib/pkgconfig $PKG_CONFIG_PATH"
