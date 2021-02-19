ff8efc8c751dd179ae3e746744541058618c8a17
Paulo S. Costa <pauloscosta5@gmail.com>
2021-02-19 13:44:29 -0800

    Rename iterm portion to run last

--- zsh/.oh-my-zsh/custom/zzz_iterm.zsh
+++ zsh/.oh-my-zsh/custom/zzz_iterm.zsh
@@ -1,4 +1,5 @@
 # iTerm integration
 # https://iterm2.com/
 # curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+# Mark appearance seems to depend on when this is run, so run it last
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
