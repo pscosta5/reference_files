3477f48d617ee9314148b1c9137d7a68f6710c9b
Paulo S. Costa <pauloscosta5@gmail.com>
2021-03-12 16:35:34 -0800

    [zsh] move default pspg args to env variable

--- zsh/.oh-my-zsh/custom/pspg.zsh
+++ zsh/.oh-my-zsh/custom/pspg.zsh
@@ -2,6 +2,6 @@
 # https://github.com/okbob/pspg
 # brew install pspg
 # Set pspg theme to simple
-alias pspg="pspg -s 16 --vertical-cursor --bold-labels --bold-cursor --force-uniborder"
-# Set as default pager for presto
-export PRESTO_PAGER='pspg -s 16 --vertical-cursor --bold-labels --bold-cursor --force-uniborder'
+export PSPG='-s 16 --vertical-cursor --bold-labels --bold-cursor --force-uniborder'
+# # Set as default pager for presto
+export PRESTO_PAGER="pspg ${PSPG}"
