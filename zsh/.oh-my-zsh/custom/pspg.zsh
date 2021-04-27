d7d3c5c264adb687dffe390ef857694cf49593f0
Paulo S. Costa <pauloscosta5@gmail.com>
2021-04-27 08:35:04 -0700

    [pspg] -c flag default

--- zsh/.oh-my-zsh/custom/pspg.zsh
+++ zsh/.oh-my-zsh/custom/pspg.zsh
@@ -2,6 +2,6 @@
 # https://github.com/okbob/pspg
 # brew install pspg
 # Set pspg theme to simple
-export PSPG='-s 16 --vertical-cursor --bold-labels --bold-cursor --force-uniborder'
+export PSPG='-s 16 --vertical-cursor --bold-labels --bold-cursor --force-uniborder -c=0'
 # # Set as default pager for presto
 export PRESTO_PAGER="pspg ${PSPG}"
