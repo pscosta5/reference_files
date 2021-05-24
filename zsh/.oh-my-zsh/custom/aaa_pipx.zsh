522a5706651ad81af48e4df7e858b76ceae1aaf4
Paulo S. Costa <pauloscosta5@gmail.com>
2021-05-24 15:31:56 -0700

    [pipx] Move pipx path to start of custom scripts

--- zsh/.oh-my-zsh/custom/aaa_pipx.zsh
+++ zsh/.oh-my-zsh/custom/aaa_pipx.zsh
@@ -6,5 +6,5 @@
 # Must run before:
 # autoload -U bashcompinit
 # bashcompinit
-export PATH="$PATH:/Users/paulo/.local/bin"
+export PATH="$PATH:/Users/${USER}/.local/bin"
 eval "$(register-python-argcomplete pipx)"
